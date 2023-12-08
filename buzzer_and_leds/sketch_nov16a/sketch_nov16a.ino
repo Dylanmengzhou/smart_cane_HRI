f0573ff0e0d3fda29cdf084567a0f40a0f341742
bimengzhou <dylanmengzhou@gmail.com>
2023-12-08 18:27:09 +0900

    add power melody

--- buzzer_and_leds/sketch_nov16a/sketch_nov16a.ino
+++ buzzer_and_leds/sketch_nov16a/sketch_nov16a.ino
@@ -1,22 +1,24 @@
 #include "pitches.h"
 #include <SoftwareSerial.h>
 
-SoftwareSerial Bluetooth(2, 3); // RX, TX
+SoftwareSerial Bluetooth(2, 3);  // RX, TX
 
 #define BUZZER_PIN 8
 #define LED_PIN1 9
 #define LED_PIN2 10
 #define BUTTON_PIN 7
 #define VIB_PIN 11
-#define BT_STATE_PIN 4  // 蓝牙模块状态引脚
-int ledFrequency, buzzerFrequency, vibFrequency; //接收数据
+#define BT_STATE_PIN 4                            // 蓝牙模块状态引脚
+int ledFrequency, buzzerFrequency, vibFrequency;  //接收数据
 int melodyConnect[] = { NOTE_C4, NOTE_E4, NOTE_G4 };
 int noteDurationsConnect[] = { 4, 4, 4 };
 int melodyDisconnect[] = { NOTE_G4, NOTE_E4, NOTE_C4 };
+int melodyBattery[] = { NOTE_D4, NOTE_C4, NOTE_C4 };
 int noteDurationsDisconnect[] = { 4, 4, 4 };
+int noteDurationBattery[] = { 4, 4, 4 };
 
-// 重新定义播放旋律的数组
-int melody[] = { NOTE_DS8 };
+  // 重新定义播放旋律的数组
+  int melody[] = { NOTE_DS8 };
 int noteDurations[] = { 8 };
 
 unsigned long previousLEDUpdate = 0;
@@ -35,6 +37,7 @@ bool isPlaying = true;
 int prevLedFrequency = -1;
 int prevBuzzerFrequency = -1;
 int prevVibFrequency = -1;
+int power = 0;
 
 
 bool lastBTState = LOW;  // 上一次蓝牙状态
@@ -51,17 +54,21 @@ void setup() {
 }
 
 void loop() {
+  if (power == 0) {
+    playTone(melodyBattery, noteDurationsDisconnect, 3);
+    power = 1;
+  }
   readButton();
   bool currentBTState = digitalRead(BT_STATE_PIN);
 
   // 检测蓝牙连接状态变化
   if (currentBTState != lastBTState) {
     if (currentBTState == HIGH) {
-      playTone(melodyConnect, noteDurationsConnect, 3); // 播放连接声音
+      playTone(melodyConnect, noteDurationsConnect, 3);  // 播放连接声音
     } else {
-      playTone(melodyDisconnect, noteDurationsDisconnect, 3); // 播放断开声音
-      stopAll(); // 蓝牙断开时，停止所有活动
-      isPlaying = false; // 更新播放状态为停止
+      playTone(melodyDisconnect, noteDurationsDisconnect, 3);  // 播放断开声音
+      stopAll();                                               // 蓝牙断开时，停止所有活动
+      isPlaying = false;                                       // 更新播放状态为停止
     }
     lastBTState = currentBTState;
   }
@@ -74,39 +81,40 @@ void loop() {
       if (c == '#') {
         break;
       } else {
-      received_string += c;
-      delay(10);
+        received_string += c;
+        delay(10);
       }
     }
     // 将接收到的字符串分割为三个数字
     parseReceivedData(received_string);
     Serial.println("DATA RECEIVED:");
     Serial.println(received_string);
-    isPlaying = true; // 在接收到新数据时重置为播放状态
+    isPlaying = true;  // 在接收到新数据时重置为播放状态
   }
-if (isPlaying && isValidFrequency(ledFrequency) && isValidFrequency(buzzerFrequency) && isValidFrequency(vibFrequency)) {
-  if (prevLedFrequency != ledFrequency || prevBuzzerFrequency != buzzerFrequency || prevVibFrequency != vibFrequency ) {
+  if (isPlaying && isValidFrequency(ledFrequency) && isValidFrequency(buzzerFrequency) && isValidFrequency(vibFrequency)) {
+    if (prevLedFrequency != ledFrequency || prevBuzzerFrequency != buzzerFrequency || prevVibFrequency != vibFrequency) {
+
+      stopAll();
+    }
+    if (ledFrequency > 0) {
+
+      blinkLEDs();
+    }
+    if (buzzerFrequency > 0) {
+
+      playMelody();
+    }
+    if (vibFrequency > 0) {
+
+      vibrateMotor();
+    }
+    prevLedFrequency = ledFrequency;
+    prevBuzzerFrequency = buzzerFrequency;
+    prevVibFrequency = vibFrequency;
+    delay(400);  // Minimum runtime
+  } else {
     stopAll();
   }
-  if (ledFrequency > 0) {
-    blinkLEDs();
-    }
-  if (buzzerFrequency > 0) {
-    playMelody();
-    }
-  if (vibFrequency > 0) {
-    vibrateMotor();
-    }
-  prevLedFrequency = ledFrequency;
-  prevBuzzerFrequency = buzzerFrequency;
-  prevVibFrequency = vibFrequency;
-  delay(400);     // Minimum runtime
-} else {
-  stopAll();
-}
-
-
-
 }
 bool isValidFrequency(int frequency) {
   return frequency >= 0 && frequency <= 8;
@@ -154,8 +162,8 @@ void playMelody() {
 
 void vibrateMotor() {
   unsigned long currentMillis = millis();
-  if (vibFrequency == 3) { // 最快频率
-    digitalWrite(VIB_PIN, HIGH); // 持续震动
+  if (vibFrequency == 3) {        // 最快频率
+    digitalWrite(VIB_PIN, HIGH);  // 持续震动
   } else if (currentMillis - previousVIBUpdate >= VIBIntervals[vibFrequency - 1]) {
     previousVIBUpdate = currentMillis;
     digitalWrite(VIB_PIN, toggleVIB ? HIGH : LOW);
